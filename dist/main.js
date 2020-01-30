1c69a00c5fea8b82f99dd4e4541af608a4706b00
amirgal <amirc538@gmail.com>
2020-01-30 16:17:53 +0200

    fixed bugs and added the priority feature

--- dist/main.js
+++ dist/main.js
@@ -4,10 +4,11 @@ const render = function (todos) {
     $("#todos").empty()
 
     todos.forEach(todo => {
+        // console.log(todo)
         $("#todos").append(`
-        <div data-id=${todo._id} class="todo ${todo.complete ? 'complete' : ''}">
+        <div data-id=${todo.id} class="todo ${todo.priority} ${todo.complete ? 'complete' : ''}">
             <i class="fas fa-check-circle"></i>
-            <span class=text>todo.text</span>
+            <span class=text>${todo.text}</span>
             <span class="delete"><i class="fas fa-trash"></i></span>
         </div>
         `)
@@ -32,9 +33,19 @@ $("#todos").on("click", ".fa-check-circle", function () {
 
 $("#todos").on("click", ".fa-trash", function () {
     const id = $(this).closest(".todo").data().id
+
     $.ajax({
         method: "DELETE",
         url: "/todo/" + id,
+        success: todos => render(todos)
+    })
+})
+//our code for priority
+$("#todos").on("click", ".todo", function () {
+    const id = $(this).data().id
+    $.ajax({
+        method: "PUT",
+        url: "/priority/" + id,
         success: todos => render(todos)
     })
 })
--- server/routes/api.js
+++ server/routes/api.js
@@ -10,7 +10,7 @@ router.get('/todos', function (req, res) {
 
 router.post('/todo', function (req, res) {
     const text = req.body.text
-    const newTodo = { id: id++, text: text, complete: false }
+    const newTodo = { id: id++, text: text, complete: false, priority: "LOW"}
 
     todos.push(newTodo)
     res.send(todos)
@@ -18,15 +18,35 @@ router.post('/todo', function (req, res) {
 
 router.put('/todo/:todoID', function (req, res) {
     const todoID = req.params.todoID
-
-    todos.find(t => t.id == todoID).completed = true
+    
+    todos.find(t => t.id == todoID).complete = 
+    !todos.find(t => t.id == todoID).complete
+    
     res.send(todos)
 })
 
 router.delete('/todo/:todoID', function (req, res) {
     const todoID = req.params.todoID
-    todos.splice(todoID, 1)
+    todos.splice(todos.findIndex(todo => todo.id == todoID), 1)
+    res.send(todos)
+})
 
+router.put('/priority/:todoID', function (req, res) {
+    const todoID = req.params.todoID
+    
+    const todoPrio = todos.find(t => t.id == todoID).priority
+    switch (todoPrio) {
+        case "LOW":
+            todos.find(t => t.id == todoID).priority = "MEDIUM"
+            break
+        case "MEDIUM":
+            todos.find(t => t.id == todoID).priority = "HIGH"
+            break
+        case "HIGH":
+            todos.find(t => t.id == todoID).priority = "LOW"
+            break
+    }
+    
     res.send(todos)
 })
 
